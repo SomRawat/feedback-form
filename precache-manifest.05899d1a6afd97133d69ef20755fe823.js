8464fcb6552bb070208480d38a6a0792ea70c384
Som Rawat <somrawat007@gmail.com>
2022-06-11 12:31:19 +0530

    1 commit

---
+++ precache-manifest.05899d1a6afd97133d69ef20755fe823.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87eaa3c6dfb7ef0d62c0ff78b6d23ae3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a66b0e577cf75a164e29",
+    "url": "/static/css/main.8ce4dcb2.chunk.css"
+  },
+  {
+    "revision": "4b33bc2b066b3dda1e9d",
+    "url": "/static/js/2.f57c980b.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/static/js/2.f57c980b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a66b0e577cf75a164e29",
+    "url": "/static/js/main.0ea81500.chunk.js"
+  },
+  {
+    "revision": "10f038249f230c0d99fb",
+    "url": "/static/js/runtime-main.fbb68eb6.js"
+  }
+]);
